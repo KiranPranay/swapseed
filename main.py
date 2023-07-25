d8a9c356b3e41370f206f9064abdfe80228d856f
kiranpranay <pranaybannu12@gmail.com>
2023-07-25 19:43:50 +0530

    Fix multiple downloads of inswapper
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ app.prepare(ctx_id=0, det_size=(640, 640))
 # Download 'inswapper_128.onnx' file using gdown
 model_url = 'https://drive.google.com/uc?id=1HvZ4MAtzlY74Dk4ASGIS9L6Rg5oZdqvu'
 model_output_path = 'inswapper/inswapper_128.onnx'
-gdown.download(model_url, model_output_path, quiet=False)
+if not os.path.exists(model_output_path):
+    gdown.download(model_url, model_output_path, quiet=False)
+
 
 swapper = insightface.model_zoo.get_model('inswapper/inswapper_128.onnx', download=False, download_zip=False)
 
